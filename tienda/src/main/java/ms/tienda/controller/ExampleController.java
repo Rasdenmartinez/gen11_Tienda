e001651f3a9103dcfb5c2b7728e33aab0758de43
Alexis7000 <fabianvelasquezalexis@gmail.com>
2024-08-13 20:02:22 -0600

    Se agreǵo comentarios en la clase ExampleController

--- tienda/src/main/java/ms/tienda/controller/ExampleController.java
+++ tienda/src/main/java/ms/tienda/controller/ExampleController.java
@@ -2,7 +2,7 @@ package ms.tienda.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
-
+//En este clase se agrega la lógica de negocio
 @RestController
 public class ExampleController {
 }
