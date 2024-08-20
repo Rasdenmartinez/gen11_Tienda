c3929db30d24709c1561e547c3f63e0ee748fac9
Alexis7000 <fabianvelasquezalexis@gmail.com>
2024-08-20 17:58:51 -0600

    uso de metodos crud y borrado logico

---
+++ tienda/src/main/java/ms/tienda/entity/Empleados.java
@@ -0,0 +1,45 @@
+package ms.tienda.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name ="empleados")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empleados {
+	
+	   @Id 
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name = "id")
+	    private Long id;
+	   
+	   @Column(name ="nombre")
+	   private String nombre;
+	   
+	   @Column(name ="apellido")
+	   private String apellido;
+	   
+	   @Column(name ="puesto")
+	   private String puesto;
+	   
+	   @Column(name ="salario")
+	   private Double salario;
+	   
+	   @Column(name ="fecha_contratacion")
+	   private LocalDateTime fechaContratacion;
+	   
+	   @Column(name ="is_active")
+	   private Boolean isActive;
+	   
+	   @Column(name = "create_date")
+	    private LocalDateTime createDate;
+
+	    @Column(name = "update_date")
+	    private LocalDateTime updateDate;
+
+}
